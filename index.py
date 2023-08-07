ff36dfee4010408a69374d60f217bfbcd5cc24aa
Clderon <luis.calderon@unas.edu.pe>
2023-08-06 22:55:01 -0500

    Subiendo funciones Pilas-List

--- index.py
+++ index.py
@@ -1,12 +1,12 @@
 # Lista de nombres de frutas
-frutas = ["manzana", "naranja", "plátano", "uva", "pera"]
+frutas = ["manzanas", "naranjas", "plátanos", "uva", "peras"]
 
 # Fruta que queremos buscar
 fruta_buscada = "uva"
 
 # Verificar si la fruta está en la lista
 if fruta_buscada in frutas:
-    # Obtener el índice de la fruta en la lista
+    # Obtener el indice de la fruta en la lista
     indice_fruta = frutas.index(fruta_buscada)
     print(f"{fruta_buscada} está en la lista en la posición {indice_fruta}.")
 else:
